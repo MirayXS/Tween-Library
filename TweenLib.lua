86520df362282e7c5a64d74e1153d8c895d2a726
ArskyCache <85579771+A1sky@users.noreply.github.com>
2021-06-26 10:42:02 -0400

    Update TweenLib.lua

--- TweenLib.lua
+++ TweenLib.lua
@@ -1,77 +1,37 @@
-local CreateLibrary = function()
-	local Library = {}
-
-	return Library
-end
-
-local TweenLib = CreateLibrary()
 local TweenService = game:GetService("TweenService")
 
-function TweenLib:TweenSize(Object, Property)
+function TweenSize(Object, Property)
 	local Tween = TweenService:Create(Object, TweenInfo.new(Property["Time"]), {Size = Property["Size"]}):Play()
 
 	return Tween
 end
 
-function TweenLib:TweenTextTransparency(Object, Property)
+function TweenTextTransparency(Object, Property)
 	local Tween = TweenService:Create(Object, TweenInfo.new(Property["Time"]), {TextTransparency = Property["Transparency"]}):Play()
 
 	return Tween
 end
 
-function TweenLib:TweenBackgroundTransparency(Object, Property)
+function TweenBackgroundTransparency(Object, Property)
 	local Tween = TweenService:Create(Object, TweenInfo.new(Property["Time"]), {BackgroundTransparency = Property["Transparency"]}):Play()
 
 	return Tween
 end
 
-function TweenLib:TweenBackgroundColor(Object, Property)
+function TweenBackgroundColor(Object, Property)
 	local Tween = TweenService:Create(Object, TweenInfo.new(Property["Time"]), {BackgroundColor3 = Property["Color"]}):Play()
 
 	return Tween
 end
 
-function TweenLib:TweenTextColor(Object, Property)
+function TweenTextColor(Object, Property)
 	local Tween = TweenService:Create(Object, TweenInfo.new(Property["Time"]), {TextColor3 = Property["Color"]}):Play()
 
 	return Tween
 end
 
---[[
-	script.Parent is the Frame its editing. So change it into the frame you wanna edit
-]]
+function TweenPosition(Object, Property)
+	local Tween = TweenService:Create(Object, TweenInfo.new(Property["Time"]), {Position = Property["Position"]}):Play()
 
---[[ Tween Size
-	TweenLib:TweenSize(script.Parent, {
-		["Time"] = 1,
-		["Size"] = UDim2.new(0, 0, 0, 0)
-	})
-]]
-
---[[ Tween Text Transparency
-	TweenLib:TweenTextTransparency(script.Parent, {
-		["Time"] = 1,
-		["Transparency"] = 0.5
-	})
-]]
-
---[[ Tween Text Transparency
-	TweenLib:TweenBackgroundTransparency(script.Parent, {
-		["Time"] = 1,
-		["Transparency"] = 0.5
-	})
-]]
-
---[[ Tween Background Color
-	TweenLib:TweenBackgroundColor(script.Parent, {
-		["Time"] = 1,
-		["Color"] = Color3.new(0, 0, 0)
-	})
-]]
-
---[[ Tween Text Color
-	TweenLib:TweenTextColor(script.Parent, {
-		["Time"] = 1,
-		["Color"] = Color3.new(0, 0, 0)
-	})
-]]
+	return Tween
+end
